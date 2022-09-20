6612e66eff60b26f1d6d5e744f0f281e5456e78f
Alteral323 <62857494+Alteral323@users.noreply.github.com>
2022-09-20 01:26:56 -0400

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+local IDs = {}
+local modules = "https://raw.githubusercontent.com/Alteral323/v/main/modules/"
+local compat = "https://raw.githubusercontent.com/Alteral323/v/main/libs/compat.lua"
+local exists = IDs[game.PlaceId]
+if exists then
+    local sandbox = loadstring(game:HttpGet(compat))()
+    sandbox(modules .. exists .. ".lua", exists)
+end
