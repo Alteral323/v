1307a9d24dad5801932ef86c355c48d30f1b9e75
Alteral323 <62857494+Alteral323@users.noreply.github.com>
2022-09-20 15:42:12 -0400

    Update evade.lua

--- modules/evade.lua
+++ modules/evade.lua
@@ -15,10 +15,7 @@ ESP:AddObjectListener(workspace.Game.Players, {
     end,
     Color = Color3.fromRGB(255, 0, 128),
     Validator = function(obj)
-        if obj:FindFirstChild("StatChanges") or obj:FindFirstChild("HRP") then
-            return true
-        end
-        return false
+        return obj:FindFirstChild("StatChanges") or obj:FindFirstChild("HRP")
     end,
     IsEnabled = "nextbotEsp"
 })
