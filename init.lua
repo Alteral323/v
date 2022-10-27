2bb35162e35291bb106f4f7d223b23f4182bf3e7
Alteral323 <62857494+Alteral323@users.noreply.github.com>
2022-10-27 17:52:22 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local IDs = {
     [3647333358] = "evade",
-    [3104101863] = "michaelszombies"
+    [3104101863] = "michaelszombies",
+    [2534724415] = "liberty"
 }
 local modules = "https://raw.githubusercontent.com/Alteral323/v/main/modules/"
 local compat = "https://raw.githubusercontent.com/Alteral323/v/main/libs/compat.lua"
