32e652b0d9be8faa8fe0c1cd272ec07b78ded794
Alteral323 <62857494+Alteral323@users.noreply.github.com>
2022-10-27 23:25:44 -0400

    Update compat.lua

--- libs/compat.lua
+++ libs/compat.lua
@@ -145,8 +145,8 @@ globals.sandbox = function(url, custom)
 			ESP.Color = ESP.Presets.Green
 			return ESP
 		end
-		globals.maid = globals.sandbox("https://raw.githubusercontent.com/Alteral323/v/main/libs/maid.lua")
-		globals.signal = globals.sandbox("https://raw.githubusercontent.com/Alteral323/v/main/libs/signal.lua")
+		globals.maid = LoadURL("https://raw.githubusercontent.com/Alteral323/v/main/libs/maid.lua")
+		globals.signal = LoadURL("https://raw.githubusercontent.com/Alteral323/v/main/libs/signal.lua")
 	end
 	local module = assert(loadstring(game:HttpGet(url)))
 	setfenv(module, setmetatable(globals, {__index = getfenv(1)}))
