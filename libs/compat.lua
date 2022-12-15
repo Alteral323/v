38577885baa6e37220635b8f8ff4de6d38032896
Alteral323 <62857494+Alteral323@users.noreply.github.com>
2022-12-14 21:31:47 -0500

    Update compat.lua

--- libs/compat.lua
+++ libs/compat.lua
@@ -148,8 +148,8 @@ globals.sandbox = function(url, custom)
 		globals.maid = LoadURL("https://raw.githubusercontent.com/Alteral323/v/main/libs/maid.lua")
 		globals.signal = LoadURL("https://raw.githubusercontent.com/Alteral323/v/main/libs/signal.lua")
 	end
-	local module = (shared.VapeDeveloper and assert(loadstring(readfile("vape/CustomModules/" .. custom .. ".lua")))) or assert(loadstring(game:HttpGet(url)))
-	shared.VapeDeveloper = nil
+	local module = (shared.VapeDeveloperMode and assert(loadstring(readfile("vape/CustomModules/" .. custom .. ".lua")))) or assert(loadstring(game:HttpGet(url)))
+	shared.VapeDeveloperMode = nil
 	setfenv(module, setmetatable(globals, {__index = getfenv(1)}))
 	return module() or {}
 end
